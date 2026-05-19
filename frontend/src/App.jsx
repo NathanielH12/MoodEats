cf13164811fd2e65804ac4a9f0d7ce903a1de614
Nathaniel <nathaniel.hendra03@gmail.com>
2026-05-19 20:54:17 +1000

    feat: add routes for register and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,16 @@ import heroImg from './assets/hero.png'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './Components/LandingPage.jsx'
 import Dashboard from './Components/Dashboard.jsx'
+import { RegisterPage } from './Components/RegisterPage.jsx'
+import { LoginPage } from './Components/LoginPage.jsx'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
